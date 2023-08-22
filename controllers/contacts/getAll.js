1bcb5aebac340feef79efa7879291e1872202cfa
barkode <konstantinks@gmail.com>
2023-08-22 19:27:56 +0100

    Update getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,12 @@ const { ctrlWrapper } = require("../../utils");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "-_id name email");
   res.status(200).json(result);
 };
 
