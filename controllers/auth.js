a95469500d787b1cb4c7e2518d171071380c9012
barkode <konstantinks@gmail.com>
2023-08-22 02:39:48 +0100

    перевірка авторизації

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,4 +18,21 @@ const register = async (req, res) => {
   });
 };
 
-module.exports = { register: ctrlWrapper(register) };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password not valid");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password not valid");
+  }
+
+  const token = "wekhkriblerb.roihtgkewl.ududnebdbf";
+  res.json({ token });
+};
+
+module.exports = { register: ctrlWrapper(register), login: ctrlWrapper(login) };
