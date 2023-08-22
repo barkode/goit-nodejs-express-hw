c65467b89e555f10a300f1fc655a1cd635790a6e
barkode <konstantinks@gmail.com>
2023-08-22 01:56:30 +0100

    Create auth.js

---
+++ routes/api/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const ctrl = require("../../controllers/auth");
+const { validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/user");
+
+const router = express.Router();
+
+// signup
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+module.exports = router;
