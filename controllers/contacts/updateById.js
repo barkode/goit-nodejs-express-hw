db257eb01942895c9d04c8fdcee1613a05a4d8be
barkode <konstantinks@gmail.com>
2023-08-22 01:56:28 +0100

    Update updateById.js

--- controllers/contacts/updateById.js
+++ controllers/contacts/updateById.js
@@ -1,4 +1,4 @@
-const Contact = require("../../models");
+const { Contact } = require("../../models/contact");
 const { HttpError, ctrlWrapper } = require("../../utils");
 
 const updateById = async (req, res) => {
