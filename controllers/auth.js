c5c7e427db544d11860601f59f66783f87287915
barkode <konstantinks@gmail.com>
2023-08-22 01:56:10 +0100

    Create auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+const { User } = require("../models/user");
+const { HttpError, ctrlWrapper } = require("../utils");
+
+const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email already exist.");
+  }
+
+  const newUser = await User.create(req.body);
+  res.status(201).json({
+    email: newUser.email,
+    name: newUser.name,
+  });
+};
+
+module.exports = { register: ctrlWrapper(register) };
