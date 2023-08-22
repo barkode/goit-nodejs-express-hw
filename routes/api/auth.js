9f9f557be17f1dec177efdb34102b0ef94deb3dd
barkode <konstantinks@gmail.com>
2023-08-22 19:28:05 +0100

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const { register, login } = require("../../controllers/auth");
-const { validateBody } = require("../../middlewares");
+const {
+  register,
+  login,
+  getCurrent,
+  logout,
+} = require("../../controllers/auth");
+const { validateBody, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 const router = express.Router();
@@ -10,5 +15,11 @@ router.post("/register", validateBody(schemas.registerSchema), register);
 
 // signin
 router.post("/login", validateBody(schemas.loginSchema), login);
+
+router.get("/current", authenticate, getCurrent);
+
+// logout
+
+router.post("/logout", authenticate, logout);
 
 module.exports = router;
