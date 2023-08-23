fd73f4d88d2bbd96e0c39f69183d22cf3118bb8f
barkode <konstantinks@gmail.com>
2023-08-23 11:28:18 +0100

    add change avatar

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const fs = require("fs/promises");
+
+const { User } = require("../../models/user");
+const { ctrlWrapper } = require("../../utils");
+
+const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const resultUpload = path.join(avatarsDir, originalname);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", originalname);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(201).json({ avatarURL });
+};
+
+module.exports = { updateAvatar: ctrlWrapper(updateAvatar) };
