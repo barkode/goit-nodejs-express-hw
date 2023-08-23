531fdbd747e3d2d0032a3c148e8e5e82657127ae
barkode <konstantinks@gmail.com>
2023-08-23 19:17:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("node:path");
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
 
@@ -11,7 +12,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use("/avatars", express.static(path.join(__dirname, "public")));
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
