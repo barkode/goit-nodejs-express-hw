14a8444a283f24cb908da472c7c9bdb60b4a5e75
barkode <konstantinks@gmail.com>
2023-08-23 02:01:26 +0100

    add def avatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
+
 const { User } = require("../../models/user");
 const { HttpError, ctrlWrapper } = require("../../utils");
 
@@ -11,7 +13,12 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email, { d: "robohash" }, false);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({ user: { email: newUser.email, name: newUser.name } });
 };
 
