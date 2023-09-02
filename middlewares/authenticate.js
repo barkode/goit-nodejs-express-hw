e326716a5b21f836b3987d2687f7e2e88c01af86
barkode <konstantinks@gmail.com>
2023-09-02 01:28:40 +0100

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,6 +4,23 @@ const { User } = require("../models/user");
 
 const { SECRET_KEY } = process.env;
 
+// without db query
+// const authenticate = async (req, res, next) => {
+//   try {
+//     const { authorization = "" } = req.headers;
+//     const [bearer, token] = authorization.split(" ");
+//     if (bearer === "Bearer" && token) {
+//       const decoded = jwt.verify(token, SECRET_KEY);
+//       req.user = decoded;
+//       next();
+//     }
+//   } catch {
+//     next(HttpError(401));
+//   }
+// };
+
+// with db query
+
 const authenticate = async (req, res, next) => {
   console.table(req);
   const { authorization = "" } = req.headers;
